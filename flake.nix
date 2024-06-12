cbdf91a8286e8d3168543f68ec56ff83e8710f44
Stepan Stipl <stepan@stipl.net>
2024-06-12 13:22:07 +0200

    feat: Add nix flake

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Command-line client for the Kraken exchange";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, }:
+    let
+      lastModifiedDate =
+        self.lastModifiedDate or self.lastModified or "19700101";
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in {
+
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+
+          clikraken = pkgs.buildPythonPackage rec {
+            pname = "clikraken";
+            name = "clikraken";
+            inherit version;
+
+            src = ./.;
+
+            checkPhase =
+              "runHook preCheck\n  ${pkgs.python3.interpreter} -m unittest\n  runHook postCheck\n";
+
+            propagatedBuildInputs = with pkgs.python3Packages; [
+              requests
+              krakenex
+              arrow
+              tabulate
+            ];
+
+            meta = with pkgs.lib; {
+              description = "Command-line client for the Kraken exchange";
+              homepage = "zertrin.org/projects/clikraken/";
+              license = licenses.asl20;
+            };
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            # The Nix packages provided in the environment
+            buildInputs = with pkgs.python3Packages; [
+              requests
+	      krakenex
+	      arrow
+	      tabulate
+	      setuptools 
+	    ] ++ [
+	      pkgs.pipenv
+	    ];
+	
+	    shellHook = ''
+	      pipenv install -e .
+	      pipenv shell
+	    '';
+          };
+        });
+    };
+}
